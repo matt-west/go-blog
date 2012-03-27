0f2943917d966cfef85c14862b63e7d49a14eae3
Matt West <matt.west@developercity.co.uk>
2012-03-27 20:05:13 +0100

    Updated master with latest changes

--- main.go
+++ main.go
@@ -18,13 +18,26 @@ type Post struct {
 	Title       string
 	Slug        string
 	Date        string
+	MachineDate string
 	Keywords    string
 	Description string
 	Tags        []string
 }
 
+type Tag struct {
+	Title string
+	Posts map[string]*Post
+}
+
+type Sidebar struct {
+	Recent []Post
+	Tags   map[string]*Tag
+	Pages  map[string]*Page
+}
+
 const assetPath = len("/")
 const pagePath = len("/page/")
+const tagPath = len("/tag/")
 const postPath = len("/")
 
 const maxPosts = 10 // Number posts to display on homepage
@@ -41,50 +54,86 @@ var postTemplates = make(map[string]*template.Template)
 // Templates
 var layoutTemplates *template.Set
 var errorTemplates *template.Set
+var sidebarAssets *Sidebar
+
+// Tags
+var tags = make(map[string]*Tag)
 
 // Init Function to Load Template Files and JSON Dict to Cache
 func init() {
-	// Parse Page JSON Dict
-	pagesRaw, _ := ioutil.ReadFile("pages/pages.json")
+	loadTemplates()
+	loadPages()
+	loadPosts()
+	loadTags()
+
+	n := 5
+
+	if len(postsJSON) < 5 {
+		n = len(postsJSON)
+	}
+
+	sidebarAssets = &Sidebar{postsJSON[0:n], tags, pages}
+}
+
+// Load The Tags Map
+func loadTags() {
+	for i := 0; i < len(postsJSON); i++ {
+
+		for t := 0; t < len(postsJSON[i].Tags); t++ {
+
+			_, ok := tags[postsJSON[i].Tags[t]]
+			if ok {
+				tags[postsJSON[i].Tags[t]].Posts[postsJSON[i].Title] = &postsJSON[i]
+			} else {
+				tagPosts := make(map[string]*Post)
+				tagPosts[postsJSON[i].Title] = &postsJSON[i]
+				tags[postsJSON[i].Tags[t]] = &Tag{postsJSON[i].Tags[t], tagPosts}
+			}
+
+		}
+	}
+}
+
+// Load Pages Dict and Templates
+func loadPages() {
+	pagesRaw, _ := ioutil.ReadFile("data/pages.json")
 	var pagesJSON []Page
 	err := json.Unmarshal(pagesRaw, &pagesJSON)
 	if err != nil {
 		panic("Could not parse Pages JSON!")
 	}
 
-	// Put Pages into pages map
 	for i := 0; i < len(pagesJSON); i++ {
 		pages[pagesJSON[i].Slug] = &pagesJSON[i]
 	}
 
-	// Parse Posts JSON Dict
-	postsRaw, _ := ioutil.ReadFile("posts/posts.json")
-	err = json.Unmarshal(postsRaw, &postsJSON)
-	if err != nil {
-		panic("Could not parse Posts JSON!")
-	}
-
-	// Put Posts into pages map
-	for i := 0; i < len(postsJSON); i++ {
-		posts[postsJSON[i].Slug] = &postsJSON[i]
-	}
-
-	// Parse and Cache Page Templates
 	for _, tmpl := range pages {
 		t := template.Must(template.ParseFile("./pages/" + tmpl.Slug + ".html"))
 		pageTemplates[tmpl.Slug] = t
 	}
+}
 
-	// Parse and Cache Posts Templates
+// Load Posts Dict and Templates
+func loadPosts() {
+	postsRaw, _ := ioutil.ReadFile("data/posts.json")
+	err := json.Unmarshal(postsRaw, &postsJSON)
+	if err != nil {
+		panic("Could not parse Posts JSON!")
+	}
+
+	for i := 0; i < len(postsJSON); i++ {
+		posts[postsJSON[i].Slug] = &postsJSON[i]
+	}
+
 	for _, tmpl := range posts {
 		t := template.Must(template.ParseFile("./posts/" + tmpl.Slug + ".html"))
 		postTemplates[tmpl.Slug] = t
 	}
+}
 
-	// Parse and Cache Layout Templates
+// Load Layout and Error Templates
+func loadTemplates() {
 	layoutTemplates = template.SetMust(template.ParseSetFiles("templates.html"))
-
-	// Parse and Cache Error Templates
 	errorTemplates = template.SetMust(template.ParseSetFiles("./errors/404.html", "./errors/505.html"))
 }
 
@@ -112,6 +161,9 @@ func pageHandler(w http.ResponseWriter, r *http.Request) {
 	// Header
 	layoutTemplates.Execute(w, "Header", p)
 
+	// Sidebar
+	layoutTemplates.Execute(w, "Sidebar", sidebarAssets)
+
 	// Page Template
 	err := pageTemplates[slug].Execute(w, nil)
 	if err != nil {
@@ -133,7 +185,7 @@ func postHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Check that the page exists and return 404 if it doesn't
+	// Check that the post exists and return 404 if it doesn't
 	_, ok := posts[slug]
 	if !ok {
 		w.WriteHeader(http.StatusNotFound)
@@ -141,14 +193,17 @@ func postHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Find the page
+	// Find the post
 	p := posts[slug]
 
 	// Header
 	layoutTemplates.Execute(w, "Header", p)
 
+	// Sidebar
+	layoutTemplates.Execute(w, "Sidebar", sidebarAssets)
+
 	// Post Template
-	err := postTemplates[slug].Execute(w, nil)
+	err := postTemplates[slug].Execute(w, p)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		errorTemplates.Execute(w, "505", nil)
@@ -173,8 +228,42 @@ func archiveHandler(w http.ResponseWriter, r *http.Request) {
 	// Header
 	layoutTemplates.Execute(w, "Header", p)
 
+	// Sidebar
+	layoutTemplates.Execute(w, "Sidebar", sidebarAssets)
+
 	// Archives
 	layoutTemplates.Execute(w, "Archive", postsJSON)
+
+	// Footer
+	layoutTemplates.Execute(w, "Footer", p)
+}
+
+func tagHandler(w http.ResponseWriter, r *http.Request) {
+	slug := r.URL.Path[tagPath:]
+	if slug == "" {
+		indexHandler(w, r)
+		return
+	}
+
+	// Check that the tag exists and return 404 if it doesn't
+	_, ok := tags[slug]
+	if !ok {
+		w.WriteHeader(http.StatusNotFound)
+		errorTemplates.Execute(w, "404", nil)
+		return
+	}
+
+	p := &Page{"/tag/" + slug, "Posts Tagged #" + slug, "", ""}
+
+	// Header
+	layoutTemplates.Execute(w, "Header", p)
+
+	// Sidebar
+	layoutTemplates.Execute(w, "Sidebar", sidebarAssets)
+
+	for _, tmpl := range tags[slug].Posts {
+		postTemplates[tmpl.Slug].Execute(w, posts[tmpl.Slug])
+	}
 
 	// Footer
 	layoutTemplates.Execute(w, "Footer", p)
@@ -186,12 +275,15 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 	// Header
 	layoutTemplates.Execute(w, "Header", p)
 
+	// Sidebar
+	layoutTemplates.Execute(w, "Sidebar", sidebarAssets)
+
 	// Show Recent Posts
 	for i, tmpl := range postsJSON {
 		if i >= maxPosts {
 			break
 		}
-		postTemplates[tmpl.Slug].Execute(w, nil)
+		postTemplates[tmpl.Slug].Execute(w, posts[tmpl.Slug])
 	}
 
 	// Footer
@@ -202,6 +294,7 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	http.HandleFunc("/archive", archiveHandler)
 	http.HandleFunc("/page/", pageHandler)
+	http.HandleFunc("/tag/", tagHandler)
 	http.HandleFunc("/assets/", assetHandler)
 	http.HandleFunc("/", postHandler)
 	http.ListenAndServe(":9981", nil)
